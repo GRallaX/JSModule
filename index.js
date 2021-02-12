2690f640cf805c296ebf59f3b47cbc87897a1764
GrallaX <alexey2141@gmail.com>
2021-02-12 03:09:20 +0200

    base logic

--- index.js
+++ index.js
@@ -34,3 +34,59 @@ const calendarWallpaper = (start, end) => {
 };
 
 calendarWallpaper(8, 17);
+
+class TimeTable {
+  constructor() {
+    this.activities = [
+      { start: 0, duration: 15, title: "Exercise" },
+      { start: 25, duration: 30, title: "Travel to work" },
+      { start: 30, duration: 30, title: "Scheduling the day" },
+      { start: 60, duration: 15, title: "Reviewing yesterday's commits" },
+      { start: 100, duration: 15, title: "Code review" },
+      { start: 180, duration: 90, title: "Lunch with John" },
+      { start: 360, duration: 30, title: "Skype call" },
+      { start: 370, duration: 45, title: "Follow up with designer" },
+      { start: 405, duration: 30, title: "Push up branch" },
+    ];
+  }
+  setActivity(start, duration, title, id = 1) {
+    this.activities.push({
+      id: id++,
+      start: start,
+      duration: duration,
+      title: title,
+    });
+  }
+}
+const renderActivity = (activities) => {
+  let i = 1;
+  for (let activity of activities) {
+    const end = (item) => item.duration + item.start;
+    activity.width = "200px";
+    activity.left = "100px";
+    for (let activityForCompare of activities) {
+      if (
+        activity.start < activityForCompare.start &&
+        activityForCompare.start < end(activity)
+      ) {
+        activity.width = "100px";
+      } else if (
+        activity.start > activityForCompare.start &&
+        activity.start < end(activityForCompare)
+      ) {
+        activity.width = "100px";
+        activity.left = activityForCompare.left === "100px" ? "200px" : "100px";
+      }
+    }
+    const activityBlock = cElem("div", "activity", activity.title);
+    activityBlock.style.height = `${activity.duration * 2}px`;
+    activityBlock.style.width = activity.width;
+    activityBlock.style.top = `${activity.start * 2}px`;
+    activityBlock.style.left = activity.left;
+    activityBlock.id = activity.id || i++;
+    calendarWrapper.append(activityBlock);
+  }
+};
+
+const schedule = new TimeTable();
+renderActivity(schedule.activities);
